8b839e8f5c76687c4e844bc8b321a85954fbe21f
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2024-10-16 14:49:00 +0300

    added copy button to  the Entry.

--- src/components/Entry/Entry.tsx
+++ src/components/Entry/Entry.tsx
@@ -1,6 +1,8 @@
 import moment from "moment";
 import { EntrySchema } from "../../types";
 import classes from "./Entry.module.scss";
+import { useRef, useState } from "react";
+import classNames from "classnames";
 
 interface EntryProps {
   entry: EntrySchema;
@@ -13,14 +15,32 @@ export default function Entry({
   onRemoveClick,
   onEditClick,
 }: EntryProps) {
+  const [copied, setCopied] = useState(false);
+
+  const contentRef = useRef<HTMLPreElement>(null);
+
+  const copyEntryContent = () => {
+    try {
+      navigator.clipboard.writeText(contentRef.current!.innerHTML);
+      setCopied(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <li className={classes.entry}>
       <header>
         <h4>{moment.utc(created_at).local().format("LLLL")}</h4>
       </header>
       <span className={classes.divider}></span>
-      <pre>{content}</pre>
+      <pre ref={contentRef}>{content}</pre>
       <span className={classes.divider}></span>
+      {updated_at ? (
+        <span className={classes.updatedAt}>
+          Updated at: {moment.utc(updated_at).local().format("LLLL")}
+        </span>
+      ) : null}
       <footer className={classes.entryFooter}>
         <div className={classes.buttons}>
           <button onClick={onRemoveClick} className={classes.deleteButton}>
@@ -29,12 +49,13 @@ export default function Entry({
           <button className={classes.editButton} onClick={onEditClick}>
             Edit entry
           </button>
+          <button
+            onClick={copyEntryContent}
+            className={classNames(classes.copy, { [classes.copied]: copied })}
+          >
+            {copied ? "Copied!" : "Copy"}
+          </button>
         </div>
-        {updated_at ? (
-          <span className={classes.updatedAt}>
-            Updated at: {moment.utc(updated_at).local().format("LLLL")}
-          </span>
-        ) : null}
       </footer>
     </li>
   );
